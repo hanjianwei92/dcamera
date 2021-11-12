1928101209a6cdc53eb576c52c651febba2afb36
Jarin Din <1121278235@qq.com>
2021-11-12 14:52:16 +0800

    add start_image_id and image_id += 1

--- dcamera/dataset_reader.py
+++ dcamera/dataset_reader.py
@@ -6,14 +6,14 @@ import time
 
 
 class DatasetReader(DCamera):
-    def __init__(self, dataset_root: str, image_id):
+    def __init__(self, dataset_root: str, start_image_id):
         super(DatasetReader, self).__init__()
         if "l515" in dataset_root:
             self.depth_scale = 0.00025
         elif "d435" in dataset_root:
             self.depth_scale = 0.001
         self.dataset_root = dataset_root
-        self.image_id = image_id
+        self.image_id = start_image_id
         self.K = np.load(os.path.join(self.dataset_root, "clb_k", str(self.image_id) + ".npy"))
 
     def open(self):
@@ -25,4 +25,5 @@ class DatasetReader(DCamera):
     def get_frame(self):
         color_image = cv2.imread(os.path.join(self.dataset_root, "imgs_rgb", str(self.image_id) + ".jpg"))
         depth_image = np.load(os.path.join(self.dataset_root, "imgs_depth", str(self.image_id) + ".npy"))
+        self.image_id += 1
         return color_image, depth_image * self.depth_scale, time.time()
