cbb726425d43889071c8e6a36d630b795351277e
Han <hanjianwei92@163.com>
2022-03-26 19:19:08 +0800

    fix spell error

--- dcamera/interface.py
+++ dcamera/interface.py
@@ -15,11 +15,6 @@ class DCamera:
         pass
 
     def get_frame(self):
-<<<<<<< HEAD
-        return None, None, time.time()
-        # rgb image, depth image, timestamp
-=======
         return None, None, time.time() # rgb image, depth image, timestamp
->>>>>>> d0d9d11803c9e780d36c487d791786e625b91dc2
 
 
