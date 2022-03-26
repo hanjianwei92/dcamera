ed2392ec18d44831b227b0e51b4d02cf187ff3b3
Jianwei Han <hanjianwei92@163.com>
2022-03-26 13:35:38 +0800

    update setup message

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import find_packages, setup
 
 setup(
     name="dcamera",
-    version="0.3",
-    author="JarintotionDin",
-    url="github.com",
-    description="dcamera interface",
+    version="0.4",
+    author="Han",
+    url="10.0.8.172",
+    description="dcamera interface (realsense mehmind)",
     packages=find_packages(exclude=("configs", "tests", "data", "scripts", "visualization")),
     data_files=[('dcamera/realsense_advance_mode_jsons',
                  ['dcamera/realsense_advance_mode_jsons/HighResHighAccuracyPreset.json',
